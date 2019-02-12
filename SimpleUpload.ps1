9ed24d75615a11ecbf8ff4e742145958a227adf7
Michael K. Sondej <michal.sondej000@gmail.com>
2019-02-12 19:45:14 +0100

    Simple upload version contained in a single file for some cases

---
+++ SimpleUpload.ps1
@@ -0,0 +1,45 @@
+﻿# A standalone version of UploadToMattermost which you can run manually from the terminal
+# does not generate public links, just uploads to the personal channel
+
+# Fill these vars:
+$apiUrl = "/api/v4";
+$login = "";
+$token = "";
+
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", "Bearer " + $cred.CredentialBlob)
+
+# Enable support for TLS
+[System.Net.ServicePointManager]::SecurityProtocol = 'Tls,Tls11,Tls12'
+    
+#Get users private channel id
+Write-Host "Pobieram dane użytkownika"
+$userData = Invoke-RestMethod -Uri "$apiUrl/users/me" -Method Get -Headers $headers
+$channelResult = Invoke-RestMethod -Uri "$apiUrl/channels/direct" -Method Post -Headers $headers -Body "[`"$($userData.id)`", `"$($userData.id)`"]"
+$channelId = $channelResult.id
+
+$fileIds = @()
+foreach($path in $paths) {
+    Write-Host "Wysyłam " $path
+
+    $channelIdEncoded = [System.Web.HttpUtility]::UrlEncode($channelId)
+    $fileName = Split-Path $path -Leaf
+    $fileNameEncoded = [System.Web.HttpUtility]::UrlEncode($fileName)
+
+    [uri]$uri = "$apiUrl/files?channel_id=$channelIdEncoded&filename=$fileNameEncoded"
+
+    $uploadResult = Invoke-RestMethod -Uri ([uri]$uri) -Method Post -Headers $headers -InFile $path -ContentType "application/octet-stream; charset=utf-8"
+
+    #solve the most annoying powershell thing (force array even on single element)
+    $uploadResult.file_infos | ForEach-Object { $fileIds += $_.id }
+}
+
+#Post a message with the file attached
+Write-Host "Robię posta"
+$postData = @{
+    "channel_id" = $channelId;
+    "message" = "#Quickshare";
+    "file_ids" = $fileIds;
+}
+$postDataJson = $postData | ConvertTo-Json
+Invoke-RestMethod -Uri "$apiUrl/posts" -Method Post -Headers $headers -Body $postDataJson
